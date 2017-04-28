90914af8b16e168b25ef2887396989e83a25f187
Chris Carey <careych5@Chriss-MacBook-Pro.local>
2017-04-28 01:51:55 -0400

    react base setup

--- dist/form.js
+++ dist/form.js
@@ -14,16 +14,16 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var Input = function (_React$Component) {
-  _inherits(Input, _React$Component);
+var Form = function (_React$Component) {
+  _inherits(Form, _React$Component);
 
-  function Input() {
-    _classCallCheck(this, Input);
+  function Form() {
+    _classCallCheck(this, Form);
 
-    return _possibleConstructorReturn(this, (Input.__proto__ || Object.getPrototypeOf(Input)).apply(this, arguments));
+    return _possibleConstructorReturn(this, (Form.__proto__ || Object.getPrototypeOf(Form)).apply(this, arguments));
   }
 
-  _createClass(Input, [{
+  _createClass(Form, [{
     key: 'render',
     value: function render() {
       return _react2.default.createElement(
@@ -35,7 +35,7 @@ var Input = function (_React$Component) {
     }
   }]);
 
-  return Input;
+  return Form;
 }(_react2.default.Component);
 
-module.exports.Input = Input;
+module.exports.Form = Form;
