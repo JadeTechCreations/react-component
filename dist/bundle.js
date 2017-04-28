a5ffe638b2407418b8269d83353fb30197e201d9
Chris Carey <careych5@Chriss-MacBook-Pro.local>
2017-04-28 00:46:57 -0400

    react base setup

--- dist/bundle.js
+++ dist/bundle.js
@@ -71,16 +71,16 @@
 
 	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-	var App = function (_React$Component) {
-	  _inherits(App, _React$Component);
+	var Input = function (_React$Component) {
+	  _inherits(Input, _React$Component);
 
-	  function App() {
-	    _classCallCheck(this, App);
+	  function Input() {
+	    _classCallCheck(this, Input);
 
-	    return _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).apply(this, arguments));
+	    return _possibleConstructorReturn(this, (Input.__proto__ || Object.getPrototypeOf(Input)).apply(this, arguments));
 	  }
 
-	  _createClass(App, [{
+	  _createClass(Input, [{
 	    key: 'render',
 	    value: function render() {
 	      return _react2.default.createElement(
@@ -92,10 +92,10 @@
 	    }
 	  }]);
 
-	  return App;
+	  return Input;
 	}(_react2.default.Component);
 
-	module.exports.App = App;
+	module.exports.Input = Input;
 
 /***/ }),
 /* 2 */
