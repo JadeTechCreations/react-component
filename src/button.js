5c23b9dc9e8c0572867f21ce2bc959bbe7b7dcc6
Chris Carey <chris.carey@availity.com>
2017-04-29 23:09:00 -0400

    react component build out

--- src/button.js
+++ src/button.js
@@ -1,16 +1,31 @@
-import React from 'react'; //eslint-disable-line
+import React, { Component, PropTypes } from 'react'; //eslint-disable-line
+import cx from 'classnames';
 
 class Button extends React.Component {
-  render() {
-    const { containerClassName, hint, ...rest } = this.props;
+  static propTypes = {
+    children: PropTypes.node,
+    errorClassName: PropTypes.string,
+    className: PropTypes.string
+  };
 
+  static contextTypes = {
+    errors: PropTypes.objectOf(PropTypes.array)
+  };
+
+  render() {
+    const { containerClassName, errorClassName, className, ...rest } = this.props;
+    const isDisabled = Object.keys(this.context.errors).length;
     return (
       <div className={containerClassName}>
         <button
+          className={cx({
+            [className]: !!className,
+            [errorClassName]: isDisabled && errorClassName
+          })}
+          disabled={isDisabled}
           type="button" {...rest}>
           Click Me {this.props.name}!
         </button>
-        {hint}
       </div>
     );
   }
