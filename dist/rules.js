a8ab26cb0f28d8c2ee6d0cbe17d7d14f4f0ca9c7
Chris Carey <chris.carey@availity.com>
2017-04-29 23:26:47 -0400

    validation rules update

--- dist/rules.js
+++ dist/rules.js
@@ -3,10 +3,42 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _validator = require("validator");
+
+var _validator2 = _interopRequireDefault(_validator);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 exports.default = {
+  email: {
+    rule: function rule(value) {
+      return _validator2.default.isEmail(value);
+    },
+    hint: function hint(value) {
+      return React.createElement(
+        "span",
+        { className: "form-error is-visible" },
+        value,
+        " is not an Email."
+      );
+    }
+  },
   required: {
     rule: function rule(value) {
       return value.toString().trim();
+    },
+    hint: function hint() {
+      return React.createElement(
+        "span",
+        { className: "form-error is-visible" },
+        "Required"
+      );
+    }
+  },
+  requiredFile: {
+    rule: function rule(value) {
+      return value.length > 0;
     },
     hint: function hint() {
       return React.createElement(
--- src/rules.js
+++ src/rules.js
@@ -1,6 +1,18 @@
+import validator from 'validator';
+
 export default {
+  email: {
+    rule: value => validator.isEmail(value),
+    hint: value => <span className="form-error is-visible">{value} is not an Email.</span>
+  },
   required: {
     rule: value => value.toString().trim(),
+    hint: () => <span className="form-error is-visible">Required</span>
+  },
+  requiredFile: {
+    rule: value => {
+      return value.length > 0;
+    },
     hint: () => <span className="form-error is-visible">Required</span>
   }
 };
